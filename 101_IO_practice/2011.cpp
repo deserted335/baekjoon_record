f99a8574b553e558d537f5da5a59440bc5377dac
deserted335 <yudongu0521@gmail.com>
2023-07-01 01:51:36 +0900

    WA

--- 101_IO_practice/2011.cpp
+++ 101_IO_practice/2011.cpp
@@ -1,33 +1,32 @@
+#include <string.h>
 #include <iostream>
+#define mod 1000000
 using namespace std;
-int DP[5001] = {0};
+ 
+int DP[5000] = {0};
 char tar[5000];
-
-int main()
+ 
+int main(void)
 {
-  int len = 0, tmp;
-  // DP[i] : possible # of decodes of 0 : i+1 string
-  // DP[i] += DP[i-1] : DP[i-1] if tar[i] is valid (tar[i] != 0)/
-  // DP[i] += DP[i-2] :  DP[i-2] if digit{DP[i-1], DP[i]} is valid (10<= # <=26)
-  cin >> tar;
-
-  while (tar[len++]) continue;
-  if(tar[0] == '0'){
-    cout << 0;
+  // reference by https://data-make.tistory.com/430
+    int i, len = 0;
+    cin >> tar;
+    
+    if (tar[0] == '0') {
+        cout << 0;
+        return 0;
+    }
+    
+    DP[0] = 1;
+    for (i = 1; i < strlen(tar); i++) {
+        if(tar[i] != '0')
+            DP[i] += DP[i - 1] % mod;
+        
+        if (tar[i - 1] == '1' || tar[i - 1] == '2' && tar[i] <= '6')
+            DP[i] += i > 1 ? DP[i - 2] % mod : 1;
+    }
+ 
+    cout << DP[strlen(tar) - 1] % mod;
+ 
     return 0;
-  }
-  DP[0] = 1; // DP[2] corner case
-  DP[1] = 1;
-
-
-  for (int i = 2; i < len; i++)
-  {
-    if (tar[i-1] != '0'){
-      DP[i] = (DP[i] + DP[i-1])%1000000;
-    }
-    if (tar[i-2] == '1' || tar[i-2] == '2' && tar[i-1] <= '6'){
-      DP[i] = (DP[i] + DP[i-2])%1000000;
-    }
-  }
-  cout << DP[len-1] % 1000000;
 }
