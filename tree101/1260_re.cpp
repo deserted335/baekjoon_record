61560c657acbe85c01f76d16fd229646cf57ae88
Labtop <ssao565@snu.ac.kr>
2023-07-03 17:42:09 +0900

    incomplete

---
+++ tree101/1260_re.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+// Q. vec<vec<int>> vs vec<list<int>>
+// Q. implement by stack vs recursion
+// Q. 
+#include <vector>
+vector<vector<int>> adj;
+vector<bool> visited;
+
+void dfs(int here){
+  visited[here] = true;
+  // What to do
+
+
+  for(int i = 0; i < adj[here].size(); i++){
+    int there = adj[here][i];
+    if(!visited[there]) dfs(there);
+  }
+}
+
+void dfsAll(){
+  visited = vector<bool>(adj.size(), false);
+  for(int i = 0; i < adj.size(); ++i){
+    if(!visited[i]) dfs(i);
+  }
+}
+
+int main(){
+
+}
